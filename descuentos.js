b984be17ca1cd281209d84c83455f34d8d81d09d
HenrySuHer <henry220797@gmail.com>
2022-02-03 23:58:11 -0600

    Cambio del input de descuento por lista de cupones

--- descuentos.js
+++ descuentos.js
@@ -6,11 +6,36 @@ const calcularPrecioConDescuento = (precio,descuento)=>{
 
 const onClickButtonPriceDiscount=()=>{
     const InputPrice = document.getElementById("InputPrice");
-    const InputDiscount = document.getElementById("InputDiscount");
+    const coupons = document.getElementById("coupons");
     const priceValue=Number(InputPrice.value);
-    const discountValue=Number(InputDiscount.value);
+    const discountValue=Number(coupons.value);
     const precioConDescuento=calcularPrecioConDescuento(priceValue,discountValue);
     const resulP=document.getElementById("ResultPrice");
     resulP.innerText =`El precio con descuento son: ${precioConDescuento}`;
 }
 
+
+const coupons = [
+    {
+        name: "BLACK FRIDAY",
+        discount: 15,
+    },
+    {
+        name: "BUEN FIN",
+        discount: 30,
+    },
+    {
+        name: "PRIME DAY",
+        discount: 25,
+    },
+];
+
+if(coupons.length>0){
+    let selectCoupons=document.getElementById("coupons");
+    for (let coupon of coupons){
+        let option=document.createElement("option");
+        option.setAttribute("value",coupon.discount);
+        option.textContent=coupon.name;
+        selectCoupons.append(option);
+    }
+}
